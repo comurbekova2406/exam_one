27f9b3ca53cc2107e08b0264226e928b35bb8d51
Cholpon Omurbekova <cholpon.omurbekova@gmail.com>
2021-11-29 20:07:19 +0600

    exercise-1

---
+++ exercise-1.py
@@ -0,0 +1,46 @@
+#Перед вами 10 программ.
+# Ответьте какими типами данных будет обладать переменная x, в конце каждой программы:
+
+# 1 ответ: str
+# x = str(int(float(5)))
+
+#2 ответ: bool
+# x = 'aa' == 'AA'
+
+#3 ответ: dict
+# x = {i: i**2 for i in range(5)}
+# print(type(x))
+
+#4 ответ:set
+# x = set(list((5, 6, 7)))
+
+#5 ответ:NoneType
+# a = {1: 1, 2: 4, 3: 9}
+# x = a.get(4)
+
+#6 ответ: NoneType
+# x = [].append('j')
+
+#7 ответ: int
+# for i in range(10):
+#     if i < 5: x = 'hello'
+#     else: x = 5
+
+#8  ответ: str
+# x = input('Enter and integer: ')
+
+#9 ответ: list
+# a = 5
+# b = [1, 3, 5, ]
+# x = 'x'
+# a, b, x = x, a, b
+
+# 10 ответ : str
+# def func(x, y=5):
+#     return x + y
+# x = func('Jaguar ', 'hunter')
+
+
+# print(type(x))
+
+
